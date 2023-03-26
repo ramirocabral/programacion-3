783bc534a452d676d2027ef156bd2b479d6b3e4b
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2023-03-26 17:21:22 -0300

    teoria clase 4

--- clase1/asd/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import Temperatura;
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(calcularCelsius(100.0));
-    }
-}
--- clase1/asd/Temperatura.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Temperatura {
-
-    static double calcularCelsius(double F){
-        return ((F - 32) * 5 / 9);
-    }
-}
