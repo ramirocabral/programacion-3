43d515e63a39d0c38acb60c9a93cc80d4e48f66a
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2023-05-29 21:37:08 -0300

    a

--- practicas/practica5/Adivinanza.java
+++ practicas/practica5/Adivinanza.java
@@ -31,51 +31,52 @@ public  class Adivinanza {
     }
 
 
-        public ListaGenericaEnlazada<ListaGenericaEnlazada<String>> secuenciaConMasPreguntas2(ArbolBinario<String> abinario) {
-          ListaGenericaEnlazada<ListaGenericaEnlazada<String>> izq, der;
-          ListaGenericaEnlazada<ListaGenericaEnlazada<String>> res = new ListaGenericaEnlazada<ListaGenericaEnlazada<String>>();
-          if (abinario.esHoja()) {
-            ListaGenericaEnlazada<String> res1 = new ListaGenericaEnlazada<String>();
-            res1.agregarInicio(abinario.getDato());
-            res.agregarInicio(res1);
-            return res;
-          }
-          izq = secuenciaConMasPreguntas2(abinario.getHijoIzquierdo());
-          der = secuenciaConMasPreguntas2(abinario.getHijoDerecho());
-          izq.comenzar();
-          der.comenzar();
-          if (izq.elemento(0).tamanio() > der.elemento(0).tamanio()) {
-            int i = 0;
-            while (izq.elemento(i)!=null) {
-              izq.elemento(i).agregarInicio("Si");
-              izq.elemento(i).agregarInicio(abinario.getDato());
-              i++;
-            }
-            return izq;
-          } else if (izq.elemento(0).tamanio() < der.elemento(0).tamanio()){
-              int i = 0;
-              while (der.elemento(i)!=null) {
-                der.elemento(i).agregarInicio("Si");
-                der.elemento(i).agregarInicio(abinario.getDato());
-                i++;
-              }
-              return der;
-            } else {
-              int i = 0;
-              while (izq.elemento(i) != null) {
-                izq.elemento(i).agregarInicio("Si");
-                izq.elemento(i).agregarInicio(abinario.getDato());
-                res.agregarFinal(izq.elemento(i));
-                i++;
-              }
-              i = 0;
-              while (der.elemento(i)!=null) {
-                der.elemento(i).agregarInicio("Si");
-                der.elemento(i).agregarInicio(abinario.getDato());
-                res.agregarFinal(der.elemento(i));
-                i++;
-              }
-            return res;
-          }
+    public static ListaGenericaEnlazada<ListaGenericaEnlazada<String>> secuenciaConMasPreguntas2(ArbolBinario<String> abinario) {
+      ListaGenericaEnlazada<ListaGenericaEnlazada<String>> izq = new ListaGenericaEnlazada<ListaGenericaEnlazada<String>>();
+      ListaGenericaEnlazada<ListaGenericaEnlazada<String>> der = new ListaGenericaEnlazada<ListaGenericaEnlazada<String>>();
+      ListaGenericaEnlazada<ListaGenericaEnlazada<String>> res = new ListaGenericaEnlazada<ListaGenericaEnlazada<String>>();
+      if (abinario.esHoja()) {
+        ListaGenericaEnlazada<String> res1 = new ListaGenericaEnlazada<String>();
+        res1.agregarInicio(abinario.getDato());
+        res.agregarInicio(res1);
+        return res;
+      }
+      if (abinario.tieneHijoIzquierdo()) {
+        izq = secuenciaConMasPreguntas2(abinario.getHijoIzquierdo());
+      }
+      if (abinario.tieneHijoDerecho()) {
+        der = secuenciaConMasPreguntas2(abinario.getHijoDerecho());
+      }
+      izq.comenzar();
+      der.comenzar();
+      if ((izq.elemento(0)!=null && der.elemento(0)!=null) && (izq.elemento(0).tamanio() > der.elemento(0).tamanio())) {
+        int i = 0;
+        while (izq.elemento(i)!=null) {
+          izq.elemento(i).agregarInicio(abinario.getDato());
+          i++;
         }
+        return izq;
+      } else if ((izq.elemento(0)!=null && der.elemento(0)!=null) && (izq.elemento(0).tamanio() < der.elemento(0).tamanio())){
+          int i = 0;
+          while (der.elemento(i)!=null) {
+            der.elemento(i).agregarInicio(abinario.getDato());
+            i++;
+          }
+          return der;
+        } else {
+          int i = 0;
+          while (izq.elemento(i) != null) {
+            izq.elemento(i).agregarInicio(abinario.getDato());
+            res.agregarFinal(izq.elemento(i));
+            i++;
+          }
+          i = 0;
+          while (der.elemento(i)!=null) {
+            der.elemento(i).agregarInicio(abinario.getDato());
+            res.agregarFinal(der.elemento(i));
+            i++;
+          }
+        return res;
+      }
+    }
 }
